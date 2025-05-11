8856606fbfb7dfa696ecb4000254be28f1798188
JedcelDeacosta <deacostajohnjedcel45@gmail.com>
2025-05-11 09:12:28 +0800

    register model on Django Admin

--- backend/backend_api/admin.py
+++ backend/backend_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, BorrowTransaction
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'copies_available')
+    search_fields = ('title', 'author', 'isbn')
+
+@admin.register(BorrowTransaction)
+class BorrowTransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'borrow_date', 'return_date', 'status')
+    list_filter = ('status',)
+    search_fields = ('user__username', 'book__title')
