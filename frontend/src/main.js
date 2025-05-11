4d5dfc70a90033ff37d653bcb410fa5b7ff6f7ec
JedcelDeacosta <deacostajohnjedcel45@gmail.com>
2025-05-11 09:17:28 +0800

    Setup main.js for frontend dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).use(router).mount('#app')
+// Configure axios
+axios.defaults.baseURL = "http://localhost:8000/api/";
+
+createApp(App).use(router).mount("#app");
